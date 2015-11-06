2172e3ee4ac28b23f8cae5297222bbca5d483f73
prespic <prespic@gmail.com>
2015-11-06 20:25:40 +0100

    Add center script

--- To_je_jedno/Assets/Scripts/CenterMovement.cs
+++ To_je_jedno/Assets/Scripts/CenterMovement.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class CenterMovement : MonoBehaviour {
+public class CenterMovement : MonoBehaviour
+{
+	private GameObject center;
+	private List<GameObject> players = new List<GameObject>();
+	private int cameraHeight;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		cameraHeight = Camera.main.pixelHeight;
+		center = GameObject.Find("/GameCenter");
+		for (int i = 1; i <= 4; i++)
+		{
+			players.Add(GameObject.Find("/F-302_" + i));
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update()
+	{
+		Vector3 calculatedPosition = new Vector3(0.0f, 0.0f, 0.0f);
+		float min = players.Min(p => p.transform.position.z);
+		float max = players.Max(p => p.transform.position.z);
+
+		calculatedPosition.z = max;
+
+		center.transform.position = calculatedPosition;
 	}
 }
