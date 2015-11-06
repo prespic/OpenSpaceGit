cf4b7c5d7c948e61cbb74332561b77e864b6a74f
Sgiath <Sgiath@outlook.com>
2015-11-06 20:19:53 +0100

    Add move scripts

--- To_je_jedno/Assets/Scripts/PlayerControll3.cs
+++ To_je_jedno/Assets/Scripts/PlayerControll3.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
-public class PlayerControll3 : MonoBehaviour {
+public class PlayerControll3 : MonoBehaviour
+{
+    private float maxTurn = 2F;
+    private float speed = 0.1F;
+    private Vector3 direction = new Vector3(0, 0, 0.1F);
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var p = speed / Math.Sqrt(direction.x * direction.x + direction.z * direction.z);
+
+        direction.x += Input.GetAxis("Horizontal3") * speed;
+
+        if (direction.x > speed * maxTurn)
+        {
+            direction.x = speed * maxTurn;
+        }
+        if (direction.x < -(speed * maxTurn))
+        {
+            direction.x = -(speed * maxTurn);
+        }
+        transform.Translate(direction * (float)p);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        direction = new Vector3();
+    }
 }
