accbf290d34be93e15f304f0f43c18c397eb6122
Sgiath <Sgiath@outlook.com>
2015-11-06 16:58:29 +0100

    Add user controller

---
+++ To_je_jedno/Assets/Controll.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controll : MonoBehaviour {
+
+    private float speed = 0.1F;
+    private Vector3 direction = new Vector3(0, 0, 0.1F);
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+        var player = GameObject.FindWithTag("Player");
+        direction.x += Input.GetAxis("Horizontal") * speed;
+        if (direction.x > speed)
+        {
+            direction.x = speed;
+        }
+        if (direction.x < -speed)
+        {
+            direction.x = -speed;
+        }
+        player.transform.Translate(direction);
+	}
+}
