46ce2bb0d6af033a20c1e9c0c460df5dc22e51d5
Sgiath <Sgiath@outlook.com>
2015-11-06 18:22:18 +0100

    Improve control

--- To_je_jedno/Assets/Controll.cs
+++ To_je_jedno/Assets/Controll.cs
@@ -1,9 +1,8 @@
 ﻿using UnityEngine;
 using System;
-using System.Collections;
 
 public class Controll : MonoBehaviour {
-
+    private float maxTurn = 2F;
     private float speed = 0.1F;
     private Vector3 direction = new Vector3(0, 0, 0.1F);
 
@@ -13,18 +12,22 @@ public class Controll : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         var p = speed / Math.Sqrt(direction.x * direction.x + direction.z * direction.z);
-        var player = GameObject.FindWithTag("Player");
 
         direction.x += Input.GetAxis("Horizontal") * speed;
 
-        if (direction.x > speed)
+        if (direction.x > speed * maxTurn)
         {
-            direction.x = speed;
+            direction.x = speed * maxTurn;
         }
-        if (direction.x < -speed)
+        if (direction.x < -(speed * maxTurn))
         {
-            direction.x = -speed;
+            direction.x = -(speed * maxTurn);
         }
-        player.transform.Translate(direction * (float) p);
+        transform.Translate(direction * (float) p);
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        direction = new Vector3();
+    }
 }
